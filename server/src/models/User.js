4dfc64ce7e1d8b89202426542d540e0c76803d3c
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-11-01 11:26:47 +0000

    User class created

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,1 +1,13 @@
 // create your User model here
+
+const { db, DataTypes, Model } = require("../db/config");
+
+class User extends Model {};
+
+User.init({
+    username: DataTypes.STRING
+}, {
+    sequelize: db
+});
+
+module.exports = User;
