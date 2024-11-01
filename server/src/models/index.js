1c85756c30651bdcb6366e3465441060762258ac
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-11-01 11:46:44 +0000

    models index created

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,14 @@
-const { User } = require('./User')
+const User = require('./User')
 // import the rest of your models above
-
+const Attack = require("./Attack");
+const Card = require("./Card");
+const Deck = require("./Deck");
 // set up the associations here
 
 // and then export them all below
-module.exports = { User }
+module.exports = {
+    User,
+    Attack,
+    Card,
+    Deck
+};
