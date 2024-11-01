0661ef4d6c37f541a759dbf88477f472335a059a
Lauren Zell-Huxley <lauren.zell-huxley@hl.co.uk>
2024-11-01 12:14:55 +0000

    associations setup

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,6 +5,15 @@ const Card = require("./Card");
 const Deck = require("./Deck");
 // set up the associations here
 
+User.hasOne(Deck);
+Deck.belongsTo(User);
+
+Deck.hasMany(Card);
+Card.belongsTo(Deck);
+
+Card.belongsToMany(Attack, {through: "CardAttack"});
+Attack.belongsToMany(Card, {through: "CardAttack"});
+
 // and then export them all below
 module.exports = {
     User,
